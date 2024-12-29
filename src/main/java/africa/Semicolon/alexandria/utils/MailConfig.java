2858d87a31b8c9214c39a47ae36c3b5e448aefdd
Orisha <orisakwenwokocha1@gmail.com>
2024-12-29 09:12:33 +0100

    Update MailConfig.java

--- src/main/java/africa/Semicolon/alexandria/utils/MailConfig.java
+++ src/main/java/africa/Semicolon/alexandria/utils/MailConfig.java
@@ -10,27 +10,39 @@ import java.util.Properties;
 @Service
 public class MailConfig {
 
+    @Value("${spring.mail.host}")
+    private String mailHost;
+
+    @Value("${spring.mail.port}")
+    private int mailPort;
+
+    @Value("${spring.mail.username}")
+    private String mailUsername;
+
+    @Value("${spring.mail.password}")
+    private String mailPassword;
+
     @Bean
     public JavaMailSender getJavaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("smtp.gmail.com");
-        mailSender.setPort(465);
+        mailSender.setHost(mailHost);
+        mailSender.setPort(mailPort);
 
-        mailSender.setUsername("orisha.spring.maail@gmail.com");
-        mailSender.setPassword("bldk vqak mdxa kqyn");
+        mailSender.setUsername(mailUsername);
+        mailSender.setPassword(mailPassword);
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+        props.put("mail.smtp.ssl.trust", mailHost);
         props.put("mail.debug", "true");
 
-        props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.socketFactory.port", "465");
+        props.put("mail.smtp.host", mailHost);
+        props.put("mail.smtp.socketFactory.port", String.valueOf(mailPort));
         props.put("mail.smtp.socketFactory.class",
                 "javax.net.ssl.SSLSocketFactory");
-        props.put("mail.smtp.port", "465");
+        props.put("mail.smtp.port", String.valueOf(mailPort));
 
         return mailSender;
     }
