b2562176638f950a6332677757fe3e0a8d1870c6
Orisha <orisakwenwokocha1@gmail.com>
2024-12-29 10:04:09 +0100

    Update MailConfig.java

--- src/main/java/africa/Semicolon/alexandria/utils/MailConfig.java
+++ src/main/java/africa/Semicolon/alexandria/utils/MailConfig.java
@@ -1,48 +1,43 @@
 package africa.Semicolon.alexandria.utils;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.mail.MailProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
-import org.springframework.stereotype.Service;
 
 import java.util.Properties;
 
-@Service
+@Configuration
 public class MailConfig {
 
-    @Value("${spring.mail.host}")
-    private String mailHost;
+    private final MailProperties mailProperties;
 
-    @Value("${spring.mail.port}")
-    private int mailPort;
-
-    @Value("${spring.mail.username}")
-    private String mailUsername;
-
-    @Value("${spring.mail.password}")
-    private String mailPassword;
+    @Autowired
+    public MailConfig(MailProperties mailProperties) {
+        this.mailProperties = mailProperties;
+    }
 
     @Bean
     public JavaMailSender getJavaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost(mailHost);
-        mailSender.setPort(mailPort);
-
-        mailSender.setUsername(mailUsername);
-        mailSender.setPassword(mailPassword);
+        mailSender.setHost(mailProperties.getHost());
+        mailSender.setPort(mailProperties.getPort());
+        mailSender.setUsername(mailProperties.getUsername());
+        mailSender.setPassword(mailProperties.getPassword());
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.ssl.trust", mailHost);
+        props.put("mail.smtp.ssl.trust", mailProperties.getHost());
         props.put("mail.debug", "true");
 
-        props.put("mail.smtp.host", mailHost);
-        props.put("mail.smtp.socketFactory.port", String.valueOf(mailPort));
-        props.put("mail.smtp.socketFactory.class",
-                "javax.net.ssl.SSLSocketFactory");
-        props.put("mail.smtp.port", String.valueOf(mailPort));
+        props.put("mail.smtp.host", mailProperties.getHost());
+        props.put("mail.smtp.socketFactory.port", mailProperties.getPort().toString());
+        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.port", mailProperties.getPort().toString());
 
         return mailSender;
     }
